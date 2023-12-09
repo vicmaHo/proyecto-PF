510d08733a0594700b676372d68b8ee508bf5a5d
Victor Hernandez <victormanu968@gmail.com>
2023-12-09 10:20:06 -0500

    ParallelCollections

---
+++ app/bin/main/taller4/Taller4.scala
@@ -0,0 +1,24 @@
+/**
+  * Taller 3 - Programación Funcional
+  * Autores: <Estudiantes>
+  * Profesor: Carlos A Delgado
+  */
+package taller4
+
+import org.scalameter.measure
+import org.scalameter.withWarmer
+import org.scalameter.Warmer
+
+object Taller4{
+
+  def saludo() = "Taller 4 2023-II"
+
+  def main(args: Array[String]): Unit = {
+    println(saludo())
+    println(
+      withWarmer(new Warmer.Default) measure {
+        (1 to 100000000).toArray
+      }
+    )
+  }
+ }
