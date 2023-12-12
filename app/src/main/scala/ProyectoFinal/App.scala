14e7aa229339e31a3d1a422a4a581446e7fad76c
JAMM0118 <jhon.alejandro.correounivalle.edu.co>
2023-12-12 01:11:58 -0500

    Solucion mejorada  y turbo implementadas

--- app/src/main/scala/ProyectoFinal/App.scala
+++ app/src/main/scala/ProyectoFinal/App.scala
@@ -16,11 +16,42 @@ object App{
     def main(args: Array[String]): Unit = {
 
     // Pruebas de la función oraculoFunc
-    val oraculo = oraculoFunc("agga")
+    val n = 11
+    val cadenaAleatoria = obj.crearADN(n)
+    println("Cadena Aleatoria: " +cadenaAleatoria)
+    println("Tamanio de la cadena: " +n)
+    //println(math.pow(4, 500))
+    //val prueba = "asda"
+    //println(prueba.length)
+
+    val oraculo = oraculoFunc(cadenaAleatoria)
+    val timeF1 = withWarmer(new Warmer.Default) measure {
+            obj.reconstruirCadenaIngenuo(cadenaAleatoria.length, oraculo)
+    }
+    val timeF2 = withWarmer(new Warmer.Default) measure {
+            obj.reconstruirCadenaMejorado(cadenaAleatoria.length, oraculo)
+    }
+    val timeF3 = withWarmer(new Warmer.Default) measure {
+        obj.reconstruirCadenaTurbo(cadenaAleatoria.length, oraculo)
+    }
+
+    val promedio = timeF1.value / timeF2.value
+    val promedio2 = timeF2.value / timeF3.value
+    
+    
     //println(obj.reconstruirCadenaIngenuo(4, oraculo))
-    val combinaciones = obj.generarCombinaciones(4)
-    println(combinaciones)
-    println(obj.reconstruirCadenaIngenuo(4, oraculo))
+    //val combinaciones = obj.generarCombinaciones(100)
+    //println(combinaciones)
+    println("Respuesta de la reconstruccion ingenua: " +obj.reconstruirCadenaIngenuo(cadenaAleatoria.length, oraculo))
+    println("Respuesta de la reconstruccion mejorada: " +obj.reconstruirCadenaMejorado(cadenaAleatoria.length, oraculo))
+    println("Respuesta de la reconstruccion turbo: " + obj.reconstruirCadenaTurbo(cadenaAleatoria.length, oraculo))
+    println()
+    println("Tiempo de la solucion ingenua: " +timeF1.value)
+    println("Tiempo de la solucion mejorada: " +timeF2.value)
+    println("Es realmente mejor la merojada: " + promedio)
+    println()
+    println("Tiempo de la solucion turbo: " +timeF3.value)
+    println("Es realmente mejor la turbo: " + promedio2)
   }
   
 }
--- app/src/main/scala/ProyectoFinal/Oraculo.scala
+++ app/src/main/scala/ProyectoFinal/Oraculo.scala
@@ -2,6 +2,7 @@ package ProyectoFinal
 
 object  Oraculo{
     type Oraculo = Seq[Char] => Boolean
+    
 
 
     def oraculoFunc(cadena: Seq[Char]): Oraculo = {
